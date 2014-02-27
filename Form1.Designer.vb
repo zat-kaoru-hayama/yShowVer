c3f0cba2fca95e2e0fff9088b41018e51267b756
HAYAMA_Kaoru <iyahaya@nifty.com>
2014-02-27 16:26:32 +0900

    Prototype (with problem: timestamp is incorrect)

---
+++ Form1.Designer.vb
@@ -0,0 +1,133 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'フォームがコンポーネントの一覧をクリーンアップするために dispose をオーバーライドします。
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Windows フォーム デザイナーで必要です。
+    Private components As System.ComponentModel.IContainer
+
+    'メモ: 以下のプロシージャは Windows フォーム デザイナーで必要です。
+    'Windows フォーム デザイナーを使用して変更できます。  
+    'コード エディターを使って変更しないでください。
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.TextBox1 = New System.Windows.Forms.TextBox()
+        Me.CheckBoxFullPath = New System.Windows.Forms.CheckBox()
+        Me.MenuStrip1 = New System.Windows.Forms.MenuStrip()
+        Me.ToolStripMenuItemAppend = New System.Windows.Forms.ToolStripMenuItem()
+        Me.ToolStripMenuItemCopy = New System.Windows.Forms.ToolStripMenuItem()
+        Me.CheckBoxSize = New System.Windows.Forms.CheckBox()
+        Me.CheckBoxMD5 = New System.Windows.Forms.CheckBox()
+        Me.MenuStrip1.SuspendLayout()
+        Me.SuspendLayout()
+        '
+        'TextBox1
+        '
+        Me.TextBox1.Anchor = CType((((System.Windows.Forms.AnchorStyles.Top Or System.Windows.Forms.AnchorStyles.Bottom) _
+                    Or System.Windows.Forms.AnchorStyles.Left) _
+                    Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
+        Me.TextBox1.BackColor = System.Drawing.SystemColors.Window
+        Me.TextBox1.Font = New System.Drawing.Font("ＭＳ ゴシック", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(128, Byte))
+        Me.TextBox1.Location = New System.Drawing.Point(12, 29)
+        Me.TextBox1.Multiline = True
+        Me.TextBox1.Name = "TextBox1"
+        Me.TextBox1.ReadOnly = True
+        Me.TextBox1.Size = New System.Drawing.Size(619, 194)
+        Me.TextBox1.TabIndex = 0
+        Me.TextBox1.TabStop = False
+        '
+        'CheckBoxFullPath
+        '
+        Me.CheckBoxFullPath.Anchor = CType((System.Windows.Forms.AnchorStyles.Bottom Or System.Windows.Forms.AnchorStyles.Left), System.Windows.Forms.AnchorStyles)
+        Me.CheckBoxFullPath.AutoSize = True
+        Me.CheckBoxFullPath.Location = New System.Drawing.Point(12, 235)
+        Me.CheckBoxFullPath.Name = "CheckBoxFullPath"
+        Me.CheckBoxFullPath.Size = New System.Drawing.Size(109, 16)
+        Me.CheckBoxFullPath.TabIndex = 2
+        Me.CheckBoxFullPath.Text = "フルパス要る？(&P)"
+        Me.CheckBoxFullPath.UseVisualStyleBackColor = True
+        '
+        'MenuStrip1
+        '
+        Me.MenuStrip1.Items.AddRange(New System.Windows.Forms.ToolStripItem() {Me.ToolStripMenuItemAppend, Me.ToolStripMenuItemCopy})
+        Me.MenuStrip1.Location = New System.Drawing.Point(0, 0)
+        Me.MenuStrip1.Name = "MenuStrip1"
+        Me.MenuStrip1.Size = New System.Drawing.Size(643, 26)
+        Me.MenuStrip1.TabIndex = 0
+        Me.MenuStrip1.TabStop = True
+        Me.MenuStrip1.Text = "MenuStrip1"
+        '
+        'ToolStripMenuItemAppend
+        '
+        Me.ToolStripMenuItemAppend.Name = "ToolStripMenuItemAppend"
+        Me.ToolStripMenuItemAppend.Size = New System.Drawing.Size(109, 22)
+        Me.ToolStripMenuItemAppend.Text = "ファイル追加(&F)"
+        '
+        'ToolStripMenuItemCopy
+        '
+        Me.ToolStripMenuItemCopy.Name = "ToolStripMenuItemCopy"
+        Me.ToolStripMenuItemCopy.Size = New System.Drawing.Size(194, 22)
+        Me.ToolStripMenuItemCopy.Text = "クリップボードへ全てコピー(&C)"
+        '
+        'CheckBoxSize
+        '
+        Me.CheckBoxSize.AutoSize = True
+        Me.CheckBoxSize.Location = New System.Drawing.Point(139, 235)
+        Me.CheckBoxSize.Name = "CheckBoxSize"
+        Me.CheckBoxSize.Size = New System.Drawing.Size(101, 16)
+        Me.CheckBoxSize.TabIndex = 3
+        Me.CheckBoxSize.Text = "サイズ要る？(&S)"
+        Me.CheckBoxSize.UseVisualStyleBackColor = True
+        '
+        'CheckBoxMD5
+        '
+        Me.CheckBoxMD5.AutoSize = True
+        Me.CheckBoxMD5.Location = New System.Drawing.Point(255, 235)
+        Me.CheckBoxMD5.Name = "CheckBoxMD5"
+        Me.CheckBoxMD5.Size = New System.Drawing.Size(121, 16)
+        Me.CheckBoxMD5.TabIndex = 4
+        Me.CheckBoxMD5.Text = "MD5SUM要る？(&M)"
+        Me.CheckBoxMD5.UseVisualStyleBackColor = True
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 12.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(643, 263)
+        Me.Controls.Add(Me.CheckBoxMD5)
+        Me.Controls.Add(Me.CheckBoxSize)
+        Me.Controls.Add(Me.CheckBoxFullPath)
+        Me.Controls.Add(Me.TextBox1)
+        Me.Controls.Add(Me.MenuStrip1)
+        Me.MainMenuStrip = Me.MenuStrip1
+        Me.MaximizeBox = False
+        Me.MinimizeBox = False
+        Me.Name = "Form1"
+        Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen
+        Me.Text = "Yet Another gui Show Version"
+        Me.MenuStrip1.ResumeLayout(False)
+        Me.MenuStrip1.PerformLayout()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents TextBox1 As System.Windows.Forms.TextBox
+    Friend WithEvents CheckBoxFullPath As System.Windows.Forms.CheckBox
+    Friend WithEvents MenuStrip1 As System.Windows.Forms.MenuStrip
+    Friend WithEvents ToolStripMenuItemAppend As System.Windows.Forms.ToolStripMenuItem
+    Friend WithEvents ToolStripMenuItemCopy As System.Windows.Forms.ToolStripMenuItem
+    Friend WithEvents CheckBoxSize As System.Windows.Forms.CheckBox
+    Friend WithEvents CheckBoxMD5 As System.Windows.Forms.CheckBox
+
+End Class
---
+++ Form1.vb
@@ -0,0 +1,103 @@
+﻿Imports System.Security.Cryptography
+
+Public Class Form1
+    Public PathList As New List(Of String)
+
+    Private stamp As New Dictionary(Of String, String)
+    Private md5cache As New Dictionary(Of String, String)
+
+    Private Shared Function VersionFilter(ByVal src As String) As String
+        Return If(src IsNot Nothing, src.Replace(",", ".").Replace(" ", ""), "(not executable)")
+    End Function
+
+    Private Shared Function Bytes2Dword(ByVal Array As Byte()) As Int64
+        Return CType(Array(0) + Array(1) * 256 + Array(2) * 256 * 256 + Array(3) * 256 * 256 * 256, Int64)
+    End Function
+
+    Private Sub UpdateTextBox()
+        Dim buffer As New System.Text.StringBuilder()
+        For Each path1 As String In PathList
+            If buffer.Length > 0 Then
+                buffer.AppendLine()
+            End If
+            '*** パス ***
+            If CheckBoxFullPath.Checked Then
+                buffer.AppendFormat("{0}{1}{2}", System.IO.Path.GetFullPath(path1), vbCrLf, vbTab)
+            Else
+                buffer.AppendFormat("{0}{1}", System.IO.Path.GetFileName(path1), vbTab)
+            End If
+
+            If System.IO.File.Exists(path1) Then
+
+                Dim vi As System.Diagnostics.FileVersionInfo =
+                    System.Diagnostics.FileVersionInfo.GetVersionInfo(path1)
+                Dim fi As New System.IO.FileInfo(path1)
+                fi.Refresh()
+
+                Dim stamp1 As String = Nothing
+                If Not stamp.TryGetValue(path1, stamp1) Then
+                    Using r = System.IO.File.OpenRead(path1)
+                        Dim array As Byte() = New Byte(4) {}
+                        r.Seek(60, IO.SeekOrigin.Begin)
+                        r.Read(array, 0, 4)
+                        Dim dword As Integer = Bytes2Dword(array)
+                        r.Seek(dword + 8, IO.SeekOrigin.Begin)
+                        r.Read(array, 0, 4)
+                        Dim datetime1 As New DateTime(Bytes2Dword(array) * 1000 * 1000 * 10, DateTimeKind.Utc)
+                        datetime1 = datetime1.AddYears(2010 - 41).ToLocalTime()
+                        stamp1 = datetime1.ToString().Replace("/", "-")
+
+                        stamp(path1) = stamp1
+                    End Using
+                End If
+
+                buffer.AppendFormat("{1}{0}{2}{0}{3}", vbTab,
+                                    VersionFilter(vi.FileVersion),
+                                    VersionFilter(vi.ProductVersion),
+                                    stamp1)
+                If CheckBoxSize.Checked Then
+                    buffer.AppendFormat("{0}{1}{2} bytes", vbCrLf, vbTab, fi.Length)
+                End If
+                If CheckBoxMD5.Checked Then
+                    If Not CheckBoxSize.Checked Then
+                        buffer.AppendLine()
+                    End If
+                    Dim md5sum As String = Nothing
+                    If Not md5cache.TryGetValue(path1, md5sum) Then
+                        Using md5_ = MD5.Create()
+                            Using stream_ = System.IO.File.OpenRead(path1)
+                                md5sum = BitConverter.ToString(md5_.ComputeHash(stream_)).Replace("-", "").ToLower()
+                            End Using
+                        End Using
+                        md5cache(path1) = md5sum
+                    End If
+                    buffer.AppendFormat("{0}md5sum:{1}", vbTab, md5sum)
+                End If
+            Else
+                buffer.Append("(file not found)")
+            End If
+        Next
+        TextBox1.Text = buffer.ToString()
+    End Sub
+
+    Private Sub CheckBoxFullPath_CheckedChanged(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles _
+        CheckBoxFullPath.CheckedChanged, CheckBoxSize.CheckedChanged, CheckBoxMD5.CheckedChanged
+        UpdateTextBox()
+    End Sub
+
+    Private Sub ToolStripMenuItemAppend_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles ToolStripMenuItemAppend.Click
+        Dim dialog As New OpenFileDialog()
+        dialog.Multiselect = True
+        If dialog.ShowDialog(Me) = Windows.Forms.DialogResult.OK Then
+            For Each path1 As String In dialog.FileNames
+                PathList.Add(path1)
+            Next
+            UpdateTextBox()
+        End If
+    End Sub
+
+    Private Sub ToolStripMenuItemCopy_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles ToolStripMenuItemCopy.Click
+        Clipboard.SetDataObject(Me.TextBox1.Text, True)
+        System.Media.SystemSounds.Asterisk.Play()
+    End Sub
+End Class
