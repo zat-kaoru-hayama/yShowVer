9ad682bd8e8ba93da154dec371cef25de51368c2
HAYAMA_Kaoru <iyahaya@nifty.com>
2017-12-08 12:39:15 +0900

    bump to 1.0.0.12

--- My Project/AssemblyInfo.vb
+++ My Project/AssemblyInfo.vb
@@ -31,5 +31,5 @@ Imports System.Runtime.InteropServices
 ' 既定値にすることができます:
 ' <Assembly: AssemblyVersion("1.0.*")> 
 
-<Assembly: AssemblyVersion("1.0.0.11")> 
-<Assembly: AssemblyFileVersion("1.0.0.11")> 
+<Assembly: AssemblyVersion("1.0.0.11")>
+<Assembly: AssemblyFileVersion("1.0.0.12")>
